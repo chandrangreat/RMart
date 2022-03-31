5341519299a0e38f3de8d4ad39eadcfd9b51c8d3
Ravi Chandran <rchandran@presidio.com>
2022-03-31 12:38:37 +0530

    Added AuthGuard

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { RouterService } from '../services/router.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanLoad {
+  constructor(private _routerService: RouterService) {}
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (!localStorage.getItem('token')) {
+      this._routerService.routeToLoginPage();
+      return false;
+    }
+    return true;
+  }
+}
