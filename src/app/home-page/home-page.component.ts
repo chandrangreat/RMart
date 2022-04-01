f816b5d342015ff1350d770907c31cb87af02d43
Ravi Chandran <rchandran@presidio.com>
2022-04-01 19:01:30 +0530

    Completed homepage

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Product } from '../core/types/Product';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit {
+  categories: Array<{ name: string; id: string }> = [
+    { name: 'All Products', id: 'all' },
+    { name: 'Shoes', id: 'shoes' },
+    { name: 'Shirts', id: 'shirts' },
+    { name: 'Pants', id: 'pants' },
+    { name: 'Jeans', id: 'jeans' },
+    { name: 'Socks', id: 'socks' },
+    { name: 'Jackets', id: 'jackets' },
+  ];
 
-  constructor() { }
+  selectedCategory: string = 'all';
+  selectedCategoryTitle: string = 'All Products';
+
+  products$: Subject<Array<Product>> = new Subject();
+
+  constructor(private _productService: ProductService) {}
 
   ngOnInit(): void {
+    this.products$ = this._productService.getAllProducts();
   }
 
+  loadProductsOfCategory(category: { name: string; id: string }): void {
+    this.products$ = this._productService.getProductsByCategory(category.id);
+    this.selectedCategory = category.id;
+    this.selectedCategoryTitle = category.name;
+  }
 }
--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { HomePageComponent } from './home-page.component';
-
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductService } from './services/product.service';
 
 @NgModule({
-  declarations: [
-    HomePageComponent
-  ],
-  imports: [
-    CommonModule,
-    HomePageRoutingModule
-  ]
+  declarations: [HomePageComponent, ProductCardComponent],
+  imports: [CommonModule, HomePageRoutingModule, HttpClientModule],
+  providers: [ProductService],
 })
-export class HomePageModule { }
+export class HomePageModule {}
