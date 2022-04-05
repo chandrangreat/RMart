ccd448a2a59c17ebc0325b91149728ef32e3bf26
Ravi Chandran <rchandran@presidio.com>
2022-04-05 19:04:24 +0530

    Added cart service

---
+++ src/app/core/services/cart.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+import { Cart, CartProduct } from '../types/Cart';
+import { Product } from '../types/Product';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  cart: Cart = { totalCartItems: 0, totalCartPrice: 0, cartproducts: [] };
+
+  cartSubject$: BehaviorSubject<Cart> = new BehaviorSubject<Cart>(this.cart);
+
+  constructor() {
+    if (!localStorage.getItem('cart')) {
+      this.initializeCartInLocalStorage();
+    } else {
+      this.cart = this.getCartFromLocalStorage();
+      this.cartSubject$.next(this.cart);
+    }
+  }
+
+  initializeCartInLocalStorage(): void {
+    this.updateCartInLocalStorage();
+  }
+
+  getCartFromLocalStorage(): Cart {
+    const cart = localStorage.getItem('cart') ?? '';
+    return JSON.parse(cart);
+  }
+
+  updateCartInLocalStorage(): void {
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+  }
+
+  updateCart(product: CartProduct, cartAction: string): void {
+    if (cartAction === 'INCREMENT') {
+      product.cartProductPrice = product.cartProductPrice + product.price;
+      product.cartProductQuantity = product.cartProductQuantity + 1;
+      this.cart.cartproducts = this.cart.cartproducts.map((cartProduct) => {
+        if (cartProduct.id === product.id) {
+          return product;
+        }
+        return cartProduct;
+      });
+      // this.cart.totalCartItems = this.cart.totalCartItems + 1;
+      this.cart.totalCartPrice = this.cart.totalCartPrice + product.price;
+      this.cartSubject$.next(this.cart);
+      this.updateCartInLocalStorage();
+    } else {
+      product.cartProductPrice = product.cartProductPrice - product.price;
+      product.cartProductQuantity = product.cartProductQuantity - 1;
+      this.cart.cartproducts = this.cart.cartproducts.map((cartProduct) => {
+        if (cartProduct.id === product.id) {
+          return product;
+        }
+        return cartProduct;
+      });
+      // this.cart.totalCartItems = this.cart.totalCartItems - 1;
+      this.cart.totalCartPrice = this.cart.totalCartPrice - product.price;
+      this.cartSubject$.next(this.cart);
+      this.updateCartInLocalStorage();
+    }
+  }
+
+  addItemToCart(product: CartProduct): void {
+    const filteredCartProducts = this.cart.cartproducts.filter(
+      (item) => item.id === product.id
+    );
+
+    if (filteredCartProducts.length > 0) {
+      this.updateCart(product, 'INCREMENT');
+    } else {
+      product.cartProductPrice = product.price;
+      product.cartProductQuantity = 1;
+      this.cart.cartproducts.push(product);
+      this.cart.totalCartItems = this.cart.totalCartItems + 1;
+      this.cart.totalCartPrice = this.cart.totalCartPrice + product.price;
+      this.cartSubject$.next(this.cart);
+      this.updateCartInLocalStorage();
+    }
+  }
+
+  getCart(): BehaviorSubject<Cart> {
+    return this.cartSubject$;
+  }
+}
