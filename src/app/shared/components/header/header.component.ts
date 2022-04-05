a4a6ccb02c2ff6ddc2e8fd577d8795117ae3420b
Ravi Chandran <rchandran@presidio.com>
2022-04-05 19:09:20 +0530

    Added shopping cart indicator in header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { CartService } from './../../../core/services/cart.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { Cart } from 'src/app/core/types/Cart';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Input() showCart: boolean = true;
+  cart$: Observable<Cart> = of();
 
-  constructor() { }
+  constructor(private _cartService: CartService) {}
 
   ngOnInit(): void {
+    if (this.showCart) {
+      this.cart$ = this._cartService.getCart().asObservable();
+    }
   }
-
 }
