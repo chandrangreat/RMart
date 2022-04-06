ae8b35b8864370d0eda214dec7808b3533bf6468
Ravi Chandran <rchandran@presidio.com>
2022-04-06 11:54:49 +0530

    Used add to cart in product modal

--- src/app/home-page/components/product-modal/product-modal.component.ts
+++ src/app/home-page/components/product-modal/product-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ContentChild } from '@angular/core';
 import { BsModalService, BsModalRef, ModalOptions } from 'ngx-bootstrap/modal';
 import { Product } from 'src/app/core/types/Product';
+import { CartService } from '../../../core/services/cart.service';
+import { CartProduct } from '../../../core/types/Cart';
 
 @Component({
   selector: 'app-product-modal',
@@ -9,7 +11,14 @@ import { Product } from 'src/app/core/types/Product';
 })
 export class ProductModalComponent implements OnInit {
   product?: Product;
-  constructor(public bsModalRef: BsModalRef) {}
+  constructor(
+    public bsModalRef: BsModalRef,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {}
+
+  additemToCart() {
+    this.cartService.addItemToCart(this.product as CartProduct);
+  }
 }
