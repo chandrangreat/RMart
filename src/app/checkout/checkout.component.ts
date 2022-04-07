3dc6568ddd80013e666fcff02e2efede48efbb6c
Ravi Chandran <rchandran@presidio.com>
2022-04-07 15:33:08 +0530

    Completed checkout page UI

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CartService } from '../core/services/cart.service';
+import { Cart, CartProduct } from '../core/types/Cart';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
+  checkoutForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    address: new FormControl('', [Validators.required]),
+    paymentMethod: new FormControl('Cash On Delivery', [Validators.required]),
+  });
 
-  constructor() { }
+  cart$: Observable<Cart> = of();
+
+  constructor(public cartService: CartService) {}
 
   ngOnInit(): void {
+    this.cart$ = this.cartService.getCart().asObservable();
   }
 
+  submitCheckoutForm() {
+    console.log(this.checkoutForm);
+  }
+
+  get name() {
+    return this.checkoutForm.get('name');
+  }
+
+  get address() {
+    return this.checkoutForm.get('address');
+  }
 }
