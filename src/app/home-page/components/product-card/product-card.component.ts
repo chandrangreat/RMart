13ffca9f16c9e3d47f486da744cb88a0b28b279b
Ravi Chandran <rchandran@presidio.com>
2022-04-07 16:43:30 +0530

    Added unsubscribe to product card component

--- src/app/home-page/components/product-card/product-card.component.ts
+++ src/app/home-page/components/product-card/product-card.component.ts
@@ -1,20 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Product } from 'src/app/core/types/Product';
 import { BsModalService, BsModalRef, ModalOptions } from 'ngx-bootstrap/modal';
 import { ProductModalComponent } from '../product-modal/product-modal.component';
 import { CartService } from '../../../core/services/cart.service';
 import { CartProduct } from '../../../core/types/Cart';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css'],
 })
-export class ProductCardComponent implements OnInit {
+export class ProductCardComponent implements OnInit, OnDestroy {
   @Input() product?: Product;
   cartProduct: CartProduct = {} as CartProduct;
   showAddToCart: boolean = true;
   bsModalRef?: BsModalRef;
+  cartServiceSubscription: Subscription | undefined;
 
   constructor(
     private modalService: BsModalService,
@@ -22,17 +24,19 @@ export class ProductCardComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.cartService.cartSubject$.asObservable().subscribe((cart) => {
-      this.cartProduct = cart.cartProducts.filter(
-        (cartProduct) => cartProduct.id === this.product?.id
-      )[0];
+    this.cartServiceSubscription = this.cartService.cartSubject$
+      .asObservable()
+      .subscribe((cart) => {
+        this.cartProduct = cart.cartProducts.filter(
+          (cartProduct) => cartProduct.id === this.product?.id
+        )[0];
 
-      if (!this.cartProduct || this.cartProduct.cartProductQuantity === 0) {
-        this.showAddToCart = true;
-      } else {
-        this.showAddToCart = false;
-      }
-    });
+        if (!this.cartProduct || this.cartProduct.cartProductQuantity === 0) {
+          this.showAddToCart = true;
+        } else {
+          this.showAddToCart = false;
+        }
+      });
   }
 
   openProductModal() {
@@ -54,5 +58,9 @@ export class ProductCardComponent implements OnInit {
 
   updateCartProductQuantity(action: string) {
     this.cartService.updateCart(this.cartProduct, action);
+  }
+
+  ngOnDestroy(): void {
+    this.cartServiceSubscription?.unsubscribe();
   }
 }
