255daf72f5abf812193fda409d770b7805e8333a
Ravi Chandran <rchandran@presidio.com>
2022-04-07 11:31:33 +0530

    Created cart quantity component

---
+++ src/app/shared/components/cart-quantity/cart-quantity.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-quantity',
+  template: `
+    <button class="btn  btn-outline-dark btn-sm" (click)="decrementQuantity()">
+      <i class="fa fa-minus" aria-hidden="true"></i>
+    </button>
+    <span class="mx-3">{{ cartProductQuantity }}</span>
+    <button class="btn  btn-outline-dark btn-sm" (click)="incrementQuantity()">
+      <i class="fa fa-plus" aria-hidden="true"></i>
+    </button>
+  `,
+  styles: [],
+})
+export class CartQuantityComponent implements OnInit {
+  @Input() cartProductQuantity?: number = 0;
+  @Output() updateCartQuantityEvent: EventEmitter<any> = new EventEmitter();
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  decrementQuantity() {
+    this.updateCartQuantityEvent.emit('DECREMENT');
+  }
+
+  incrementQuantity() {
+    this.updateCartQuantityEvent.emit('INCREMENT');
+  }
+}
