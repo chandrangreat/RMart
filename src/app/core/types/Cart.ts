652ab619c9cb0af0f98880f933d0ec21e1ec725d
Ravi Chandran <rchandran@presidio.com>
2022-04-08 15:49:46 +0530

    Removed CartProduct type

--- src/app/core/types/Cart.ts
+++ src/app/core/types/Cart.ts
@@ -1,12 +1,7 @@
 import { Product } from './Product';
 
-export interface CartProduct extends Product {
-  cartProductPrice: number;
-  cartProductQuantity: number;
-}
-
 export interface Cart {
   totalCartPrice: number;
   totalCartItems: number;
-  cartProducts: Array<CartProduct>;
+  cartProducts: Array<Product>;
 }
